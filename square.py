86374f83e6d2a1cc64350b4d5924945b6f977d41
mahamgithubmahar <mahamgithub@gmail.com>
2024-11-17 17:51:41 +0400

    turtle

---
+++ square.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.Screen().bgcolor("orange")
+sc=turtle.Screen()
+sc.setup(400,300)
+turtle.title("Welcome to the turtle window")
+board=turtle.Turtle()
+for i in range(4):
+    board.forward(100)
+    board.left(90)
+    i+=1
+turtle.done()
